641f80a2e12a38769a8fe0489d13a676c4663707
EUNSOL0313 <liat0313@gmail.com>
2024-04-07 02:47:17 +0900

    링크 수정

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -5,7 +5,7 @@ import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import { Outlet } from 'react-router-dom' //자손들 가져오는 것
+import { Outlet, Link } from 'react-router-dom' //자손들 가져오는 것
 import { ReactComponent as Logo } from '../assets/images/logo.svg'
 
 const AppLayout = () => {
@@ -13,14 +13,20 @@ const AppLayout = () => {
       <div className="wrap">
          <Navbar expand="lg" className="bg-body-tertiary">
             <Container fluid>
-               <Navbar.Brand href="/">
-                  <Logo className="logo" />
-               </Navbar.Brand>
+               <Link to={'/'} className="nav-logo-link">
+                  <Navbar.Brand href="#">
+                     <Logo className="logo" />
+                  </Navbar.Brand>
+               </Link>
                <Navbar.Toggle aria-controls="navbarScroll" />
                <Navbar.Collapse id="navbarScroll">
                   <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-                     <Nav.Link href="/">Home</Nav.Link>
-                     <Nav.Link href="/movies">Movies</Nav.Link>
+                     <Link to={'/'} className="nav-link">
+                        Home
+                     </Link>
+                     <Link to={'/movies'} className="nav-link">
+                        Movies
+                     </Link>
                   </Nav>
                   <Form className="d-flex">
                      <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
