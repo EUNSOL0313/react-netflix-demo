40bccc405332bf5caa4c80f102c109d4f4777ae1
EUNSOL0313 <liat0313@gmail.com>
2024-04-07 01:51:22 +0900

    nav 배경 수정

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -14,7 +14,6 @@ const AppLayout = () => {
          <Navbar expand="lg" className="bg-body-tertiary">
             <Container fluid>
                <Navbar.Brand href="/">
-                  {/* <img width={100} className="logo" src="https://bi-jingo.com/wp-content/uploads/2009/03/netflix-logo-png-clip-art.png" alt="logo" /> */}
                   <Logo className="logo" />
                </Navbar.Brand>
                <Navbar.Toggle aria-controls="navbarScroll" />
